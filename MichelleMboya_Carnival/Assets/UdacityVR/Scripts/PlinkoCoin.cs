dfc180cfa13d9ba3cd4682b9fb610c54d651e97f
michellemboya <michelle.mboya1@gmail.com>
2017-10-27 08:11:32 +0300

    First Commit
    Smaller files upload (MichelleCarnival)

---
+++ MichelleMboya_Carnival/Assets/UdacityVR/Scripts/PlinkoCoin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlinkoCoin : MonoBehaviour {
+
+    private GvrAudioSource ding;
+    private Rigidbody rBody;
+    private CarnivalPlinko myBoard;
+
+    private bool firstHitBottom = true;
+
+    void Start() {
+        ding = GetComponent<GvrAudioSource>();
+        rBody = GetComponent<Rigidbody>();
+        rBody.isKinematic = true;
+    }
+
+    public void DropCoin(CarnivalPlinko plinko) {
+        rBody.isKinematic = false;
+        myBoard = plinko;
+    }
+
+    public void OnCollisionStay(Collision collision) { //we use stay since we are using one collision mesh
+        if (collision.impulse.magnitude > .4f) {
+            ding.Play();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (firstHitBottom) {
+            firstHitBottom = false;
+            myBoard.CoinHitBottom(float.Parse(other.gameObject.GetComponentInChildren<TMPro.TextMeshPro>().text) );
+            StartCoroutine(DeleteAfterSeconds(5f));
+        }
+    }
+
+    IEnumerator DeleteAfterSeconds(float seconds) {
+        yield return new WaitForSeconds(seconds);
+        Destroy(this.gameObject);
+    }
+}
---
+++ MichelleMboya_Carnival/README.md
@@ -0,0 +1,3 @@
+## Versions
+- Unity 2017.1.0
+- GVR Unity SDK v1.60.0
